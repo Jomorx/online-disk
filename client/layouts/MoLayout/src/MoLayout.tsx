7faf35c1663e022bd43d364134f002a4103b6c7d
huchanghao <2955568032@qq.com>
2022-12-28 23:13:08 +0800

    WIP: add dark Mode

--- client/layouts/MoLayout/src/MoLayout.tsx
+++ client/layouts/MoLayout/src/MoLayout.tsx
@@ -1,21 +1,27 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode, useState } from "react";
 import { MoAside } from "@/layouts/MoAside";
 import { MoHeader } from "@/layouts/MoHeader";
 import styles from "./MoLayout.module.scss";
 
-const index: React.FC<{ children: ReactNode }> = ({ children }) => {
+const MoLayout: React.FC<{ children: ReactNode }> = ({ children }) => {
+  const [darkTheme, setDarkTheme] = useState<boolean>(false);
   return (
-    <div>
-      <header className={styles["layout-header"]}>
-        <MoHeader />
-      </header>
-
-      <main>
+    <div className={`App ${styles.layout}${darkTheme ? " dark" : ""}`}>
+      <aside className={styles.aside}>
         <MoAside />
+      </aside>
+      <main className={styles.main}>
+        <header className={styles.header}>
+          <MoHeader />
+          <button type="button" onClick={() => setDarkTheme(!darkTheme)}>
+            darkTheme
+          </button>
+        </header>
+
         {children}
       </main>
     </div>
   );
 };
 
-export default index;
+export default MoLayout;
